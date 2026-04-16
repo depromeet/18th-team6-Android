4ada0077ecd420a0c84bfa97e75025c759bb38bb
Thirfir <sangbbang31@gmail.com>
2026-04-16 21:29:40 +0900

    chore: Orbit 라이브러리 의존성 추가

--- android/feature/login/build.gradle.kts
+++ android/feature/login/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.testExt.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.orbit.core)
+    implementation(libs.orbit.viewmodel)
+    implementation(libs.orbit.compose)
 }
