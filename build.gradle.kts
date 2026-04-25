4e6ac65caa4d5e8dacf1251852d0249768be2ad1
ThirFir <thirfir@naver.com>
2026-04-25 17:36:50 +0900

    chore: detekt 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,39 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import org.gradle.api.Project
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.detekt)
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ktlint)
 }
 
+fun Project.configureDetekt() {
+    extensions.configure<DetektExtension>("detekt") {
+        baseline = file("detekt-baseline.xml")
+        buildUponDefaultConfig = true
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        source.setFrom(files("src"))
+    }
+
+    tasks.withType<Detekt>().configureEach {
+        include("**/*.kt", "**/*.kts")
+        exclude("**/build/**")
+    }
+}
+
+configureDetekt()
+
 subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configureDetekt()
 }
