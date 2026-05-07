dd79e19b71eb99e57ba4176d33b2c5d089c5c3cd
Ddudduu <senta2006@naver.com>
2026-05-08 01:20:57 +0900

    fix: ModalBottomSheet 제거

--- android/core/designsystem/src/main/java/com/obrit/android/core/designsystem/component/bottomsheet/OBRitBottomSheet.kt
+++ android/core/designsystem/src/main/java/com/obrit/android/core/designsystem/component/bottomsheet/OBRitBottomSheet.kt
@@ -10,10 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.SheetState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,27 +22,6 @@ import com.obrit.android.core.designsystem.theme.OBRitTheme
 import com.obrit.obrit.shared.designsystem.tokens.atom.color.AtomColors
 import com.obrit.obrit.shared.designsystem.tokens.atom.radius.AtomRadius
 import com.obrit.obrit.shared.designsystem.tokens.atom.spacing.AtomSpacing
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun OBRitModalBottomSheet(
-    onDismissRequest: () -> Unit,
-    modifier: Modifier = Modifier,
-    sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
-    content: @Composable ColumnScope.() -> Unit,
-) {
-    ModalBottomSheet(
-        onDismissRequest = onDismissRequest,
-        modifier = modifier,
-        sheetState = sheetState,
-        containerColor = Color.Transparent,
-        scrimColor = OBRitBottomSheetScrimColor,
-        dragHandle = null,
-        tonalElevation = 0.dp,
-    ) {
-        OBRitBottomSheet(content = content)
-    }
-}
 
 @Composable
 fun OBRitBottomSheet(
