2ffc1cb9c8e530bba43f4e84bf7f967e627e74b8
Choi Jiwoo <96974725+elaus00@users.noreply.github.com>
2026-05-25 20:29:56 +0900

    refactor: iOS 초기화와 홈 로딩 시작점 분리

--- iosApp/iosApp/App/iOSApp.swift
+++ iosApp/iosApp/App/iOSApp.swift
@@ -1,18 +1,10 @@
-import Shared
 import SwiftUI
 
 @main
 struct iOSApp: App {
-    private let dependencies: AppDependencies
-
-    init() {
-        SharedKoinKt.startSharedKoin()
-        dependencies = .live
-    }
-
     var body: some Scene {
         WindowGroup {
-            ContentView(dependencies: dependencies)
+            AppBootstrapView()
         }
     }
 }
