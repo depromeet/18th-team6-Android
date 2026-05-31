d7bb21918879ab3edd38dabe0f2e0776ea99e739
ThirFir <thirfir@naver.com>
2026-05-31 11:35:01 +0900

    chore: ktLint 포맷

--- android/app/src/main/kotlin/com/obrit/obrit/OBRitApplication.kt
+++ android/app/src/main/kotlin/com/obrit/obrit/OBRitApplication.kt
@@ -14,7 +14,7 @@ class OBRitApplication : Application() {
 
         initKoin {
             androidContext(this@OBRitApplication)
-            modules(agentFeatureModule,homeFeatureModule, registerFeatureModule)
+            modules(agentFeatureModule, detailFeatureModule, homeFeatureModule, registerFeatureModule)
         }
     }
 }
