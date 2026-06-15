ce6d2051cc8de8b1ca9ac14171eafd9c59fef0b3
Ddudduu <senta2006@naver.com>
2026-06-16 01:52:14 +0900

    style: ktlint 수정

--- android/feature/home/src/main/java/com/obrit/feature/home/di/HomeModule.kt
+++ android/feature/home/src/main/java/com/obrit/feature/home/di/HomeModule.kt
@@ -5,7 +5,6 @@ import com.obrit.feature.home.data.SearchHistoryDataSource
 import com.obrit.feature.home.viewmodel.HomeViewModel
 import com.obrit.feature.home.viewmodel.SearchViewModel
 import org.koin.android.ext.koin.androidContext
-import com.obrit.feature.home.viewmodel.HomeViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
